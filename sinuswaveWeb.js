ee2487a6a6ed4d1d2637288a5da7dae29794e089
Lucas Stauder <lucas@stauders.de>
2018-04-29 08:41:58 +0200

    s

--- sinuswaveWeb.js
+++ sinuswaveWeb.js
@@ -32,13 +32,20 @@ function draw() {
   for(i=0;i<osziLength;i++){
     sumWave.oszilators[i]= secondWave.oszilators[i]+firstWave.oszilators[i];
   }
-  fill(250, 0, 0);
+  
   var i;
   for(i = 1; i < osziLength+1; i++){
+      fill(250, 0, 0);
       ellipse((i-1) + xOffset*(i-1), firstWave.oszilators[i-1]+firstWave.yOffset,5,5);
+      fill(0, 0, 250);
       ellipse((i-1) + xOffset*(i-1), secondWave.oszilators[i-1]+secondWave.yOffset,5,5);
+      fill(250,0,250);
       ellipse((i-1) + xOffset*(i-1), sumWave.oszilators[i-1]+sumWave.yOffset,5,5);
   }
+  fill(0);
+  line(0, firstWave.yOffset, width, firstWave.yOffset);
+  line(0, secondWave.yOffset, width, secondWave.yOffset);
+  line(0, sumWave.yOffset, width, sumWave.yOffset);
   displayTime.html("Zeit :"+time.toFixed(2));
   if(!pause){
     time+=timeSteps;
